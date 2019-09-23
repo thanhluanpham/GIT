b84ad612a99d47516839e2f5ddf5e5ce1f79d470
plh81hc <thanhluanpham@live.com>
2019-09-23 09:09:56 +0700

    New Idea

---
+++ RTC_I2C_calibration/Debug/Src/ssd1306_fonts.d
@@ -0,0 +1,4 @@
+Src/ssd1306_fonts.o: ../Src/ssd1306_fonts.c \
+ C:/Users/Luan_Pham/Desktop/RTC_I2C_calibration/Inc/ssd1306_fonts.h
+
+C:/Users/Luan_Pham/Desktop/RTC_I2C_calibration/Inc/ssd1306_fonts.h:
